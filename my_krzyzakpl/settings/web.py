4d5a27585a3a82b4cdd348b85c984bc25b6780bf
Radek Krzyzak <krzyzak21@gmail.com>
2021-03-02 04:03:36 +0000

    no commit

--- my_krzyzakpl/settings/web.py
+++ my_krzyzakpl/settings/web.py
@@ -5,5 +5,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["www.krzyzak21.pl", "krzyzak21.pl"]
 
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
