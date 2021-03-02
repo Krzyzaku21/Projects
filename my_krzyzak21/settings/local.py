b2196189aa53c7f58b67dbab6f3b66902908bd30
Radek Krzyzak <krzyzak21@gmail.com>
2021-03-02 12:36:40 +0000

    add my_krzyzak21 website

---
+++ my_krzyzak21/settings/local.py
@@ -0,0 +1,6 @@
+#python manage.py runserver --settings settings.local
+from my_krzyzak21.settings import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
---
+++ my_krzyzak21/settings/web.py
@@ -0,0 +1,6 @@
+#python manage.py runserver --settings settings.web
+from my_krzyzak21.settings import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ["www.krzyzak21.space", "krzyzak21.space"]
