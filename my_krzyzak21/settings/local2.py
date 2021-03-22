b00d813e601bdaf7eec0b48bfe6ca085c5d5957b
Krzyzaku21 <krzyzak21@gmail.com>
2021-03-22 12:41:25 +0100

    push

--- my_krzyzak21/settings/local2.py
+++ my_krzyzak21/settings/local2.py
@@ -23,5 +23,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
